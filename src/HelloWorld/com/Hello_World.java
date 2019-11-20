e58451a84867811cdfec72e9d65ac8f26e221199
ansroid <you@example.com>
2019-11-20 03:15:33 +0100

    First initial commit

---
+++ src/HelloWorld/com/Hello_World.java
@@ -0,0 +1,11 @@
+package HelloWorld.com;
+
+public class Hello_World
+{
+    String name = "Hello World !";
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+
+    }
+}
